3181535d08343e4f74fa70241e4f6c02b20dc352
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-05-30 18:20:31 +0530

    Hardcode role to codebuils project

--- terraform/module/utilities/utilities.tf
+++ terraform/module/utilities/utilities.tf
@@ -387,87 +387,13 @@ resource "aws_cloudwatch_event_target" "event_target" {
 }
 */
 
-## Code Build Role
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "codeBuildRoleECS" {
-  name               = "codeBuildRoleECS"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "codeBuildRoleECSPolicyDoc" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "ec2:CreateNetworkInterface",
-      "ec2:DescribeDhcpOptions",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeVpcs",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:CreateNetworkInterfacePermission"]
-    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "ec2:Subnet"
-
-      values = aws_subnet.private_subnets.ids
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "ec2:AuthorizedService"
-      values   = ["codebuild.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy" "codeBuildRoleECSPolicy" {
-  role   = aws_iam_role.codeBuildRoleECS.name
-  policy = data.aws_iam_policy_document.codeBuildRoleECSPolicyDoc.json
-}
-
-
 ## Code Build
 resource "aws_codebuild_project" "codebuild" {
   name          = "ECS_Build"
   description   = "ECS_Build Codebuild Project"
   build_timeout = "5"
   ## HARD Coded role
-  service_role  = aws_iam_role.codeBuildRoleECS.arn
+  service_role  = "arn:aws:iam::598792377165:role/service-role/codebuild-MyNewBuildProject-service-role"
 
   artifacts {
     type = "S3"
