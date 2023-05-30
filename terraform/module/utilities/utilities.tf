84276453ad29141dfc9702be3cab5d34480a0d37
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-05-30 11:49:27 +0530

    Changes

--- terraform/module/utilities/utilities.tf
+++ terraform/module/utilities/utilities.tf
@@ -85,7 +85,48 @@ resource "aws_iam_role_policy" "EcsTaskExcPolicyRoleAttach_policy" {
 })
 }
 
-
+## Apply bucket policy to the bucket
+resource "aws_s3_bucket_policy" "access_logs_policy" {
+    bucket = aws_s3_bucket.lb_logs.id
+    policy = jsonencode({
+    Version: "2012-10-17",
+    Id: "AWSConsole-AccessLogs-Policy-1668059634986",
+    Statement: [
+        {
+            Sid: "AWSConsoleStmt-1668059634986",
+            Effect: "Allow",
+            Principal: {
+                AWS: "${data.aws_elb_service_account.service_account_id.arn}"
+            },
+            Action: "s3:PutObject",
+            Resource: "${aws_s3_bucket.lb_logs.arn}/AWSLogs/${data.aws_caller_identity.caller_identity.account_id}/*"
+        },
+        {
+            Sid: "AWSLogDeliveryWrite",
+            Effect: "Allow",
+            Principal: {
+                Service: "delivery.logs.amazonaws.com"
+            },
+            Action: "s3:PutObject",
+            Resource: "${aws_s3_bucket.lb_logs.arn}/AWSLogs/${data.aws_caller_identity.caller_identity.account_id}/*",
+            Condition: {
+                StringEquals: {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        },
+        {
+            Sid: "AWSLogDeliveryAclCheck",
+            Effect: "Allow",
+            Principal: {
+                "Service": "delivery.logs.amazonaws.com"
+            },
+            Action: "s3:GetBucketAcl",
+            Resource: "${aws_s3_bucket.lb_logs.arn}"
+        }
+    ]
+    })
+}
 
 # Create ECR repository for the image to store
 resource "aws_ecr_repository" "project_repo" {
